579a44dddda63c5ed02777852b962e092100053f
Juan Maya <carlosmaya326@gmail.com>
2022-09-19 19:38:45 -0500

    CRUD FAVORITOS

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,7 @@ Route::middleware(['auth:api', 'rol'])->group(function() {
         Route::resource('/reservas', ReservaController::class);
         Route::resource('/pedidos', PedidoController::class);
         Route::resource('/favoritos', FavoritosController::class);
+        Route::get('/favoritos/usuario/{id}', [FavoritosController::class,'showByUser']);
         Route::get('/perfil', [UserController::class, 'show']);
     });
 
